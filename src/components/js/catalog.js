3a558626002c5678e60bae4368c933aaf979e239
ivan kov <63105949+IKolyas@users.noreply.github.com>
2020-07-21 02:15:33 +0500

    comm

--- src/components/js/catalog.js
+++ src/components/js/catalog.js
@@ -1,4 +1,3 @@
-// Product catalog
 function createItem(id, name, price, img) {
     return { id, name, price, img }
 }
@@ -22,39 +21,61 @@ function initCatalog() {
     return names.map((name, index) => createItem(ids[index], name, prices[index], images[index]));
 }
 
-const basket = {
-    items: [],
-    add(item) {
-        //добавление товара в items корзины
-    },
-    remove(item) {
-        //удаление товара в items корзины
-    }
-};
-
 const catalog = {
     items: [],
     container: null,
     slBegin: undefined,
     slEnd: undefined,
+    basket: basket,
     init(tagID, slBegin, slEnd) {
         this.slBegin = slBegin;
         this.slEnd = slEnd;
         this.container = document.querySelector(tagID);
         this.items = initCatalog();
         this._render();
+        this._handleActions();
+    },
+    _handleActions() {
+        if (this.container){
+            this.container.addEventListener('click', evt => {
+                if (evt.target.name == 'add') {
+                    let item = {
+                        name: evt.target.dataset.name,
+                        price: +evt.target.dataset.price,
+                        img: evt.target.dataset.img,
+                        amount: 1,
+                        id: evt.target.dataset.id
+                    }
+                    this.basket.add(item);
+                }
+            })
+        }
     },
     _render() {
         let str = '';
         this.items.slice(this.slBegin, this.slEnd).forEach(item => {
             str += ` <div class="prodCard card col-12 col-md-6 col-lg-4 border-0 mx-0 px-0 mb-5">
                         <div class="psevProdCard mx-0 px-0">
-                            <a href="#" class="d-flex justify-content-around"><i class="fas fa-cart-plus"></i>Add to Cart</a>
+                            <button class="d-flex justify-content-around"
+                            name="add"
+                            data-name="${item.name}"
+                            data-img="${item.img}"
+                            data-price="${item.price}"
+                            data-id="${item.id}">
+                            <i class="fas fa-cart-plus"></i>Add to Cart
+                            </button>
                         </div>
                         <img src="${item.img}" class="card-img-top" alt="...">
                         <div class="psevProdCardBody card-body d-flex flex-column align-content-start pb-0 px-0">
-                            <h3 class="card-text px-3">${item.name}</h3>
-                            <p class="d-flex justify-content-between px-3">$${item.price}<a href="#"><span class="psevAdd d-md-none">
+                            <a href="#" class="card-text px-3">${item.name}</a>
+                            <p class="d-flex justify-content-between px-3">$${item.price}
+                            <a href="#"
+                            name="add"
+                            data-name="${item.name}"
+                            data-img="${item.img}"
+                            data-price="${item.price}"
+                            data-id="${item.id}"
+                            ><span class="psevAdd d-md-none">
                             <i class="fas fa-cart-plus"></i></span></a></p>
                         </div>
                     </div> `
@@ -75,14 +96,12 @@ catalog.init('#single_catalog', 0, 4)
 
 // openCart
 function openCart() {
-    let dropdownOn = document.querySelector('.drMyAccount');
+    let dropdownOn = document.querySelector('.basket__Product');
     document.addEventListener('click', evt => {
         if (evt.target.classList.contains('my__cart')) {
                 dropdownOn.classList.toggle('show');
             }
-        else {
-            dropdownOn.classList.remove('show');
-        }
+
     })
 }
 let open = openCart()
